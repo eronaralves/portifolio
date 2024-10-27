f4d9d694789ac057d912e1c5ec8cfc06c05b838e
eronaralves <alveseronar@gmail.com>
2024-10-27 15:18:45 -0300

    feat: images in assets

--- src/ultils/data-projects.ts
+++ src/ultils/data-projects.ts
@@ -1,9 +1,19 @@
+import type { StaticImageData } from "next/image";
 import { IconsTecnologys } from "./icon-tecnologys";
+
+// Images
+import LoginLeSelectImg from "@/assets/images/login-desafio.png"
+import ScheduleEventImg from "@/assets/images/schedule-event.png"
+import PokelistImg from "@/assets/images/background-pokelist.jpeg"
+import CoffeDeliveryImg from "@/assets/images/coffe-delivery.png"
+import PomodoroImg from "@/assets/images/pomodoro.png"
+import PageVnwImg from "@/assets/images/page-vnw.png"
+
 
 export interface Project {
   name: string;
   description: string;
-  image: string;
+  image: StaticImageData;
   languages: (keyof typeof IconsTecnologys)[];
   url_site?: string;
   year: number;
@@ -13,7 +23,7 @@ export interface Project {
 const dataProjects: Project[] = [
   {
     name: "Login Le Select",
-    image: "/images/login-desafio.png",
+    image: LoginLeSelectImg,
     description:
       "Neste projeto, realizei um teste técnico para uma oportunidade de freelancer, onde desenvolvi um sistema de login completo com integração com uma API. Através dessa implementação que demonstrei habilidades em autenticação e gerenciamento de usuários. O sucesso desse projeto foi um fator decisivo que me permitiu conquistar meu primeiro trabalho como freelancer.",
     languages: ["react", "styled_components", "typescript" ],
@@ -22,7 +32,7 @@ const dataProjects: Project[] = [
   },
   {
     name: "Agendamento descomplicado",
-    image: "/images/schedule-event.png",
+    image: ScheduleEventImg,
     description:
       "Neste projeto, durante um curso, aprendi a integrar um sistema com o Google Agenda, criando um calendário dinâmico que exibe os horários de agendas criadas, além de conseguir marcar suas reuniões.",
     languages: ["next", "typescript", "prisma"],
@@ -31,16 +41,16 @@ const dataProjects: Project[] = [
   },
   {
     name: "Pokelist",
-    image: "/images/background-pokelist.jpeg",
+    image: PokelistImg,
     description:
-      "Este projeto é uma Pokedex desenvolvida em React Native, onde os usuários podem buscar Pokémons consumindo dados de uma API. A aplicação permite filtrar os Pokémon por nome, alternar o idioma entre português e inglês, e oferece suporte ao modo escuro para melhorar a experiência do usuário em diferentes condições de luminosidade.",
+      "Este projeto é uma Pokedex desenvolvida em React Native, onde os usuários podem buscar Pokémons consumindo dados de uma API de pokémons. A aplicação permite filtrar os Pokémon por nome, alternar o idioma entre português e inglês, e oferece suporte ao modo escuro para melhorar a experiência do usuário em diferentes condições de luminosidade.",
     languages: ["react_navite", "typescript"],
     url_site: "/videos/pokelist.mp4",
     year: 2023,
   },
   {
     name: "Coffe Delivery",
-    image: "/images/coffe-delivery.png",
+    image: CoffeDeliveryImg,
     description:
       "Projeto de e-commerce para uma cafeteria. O objetivo foi criar uma plataforma intuitiva com foco de implementar uma interface funcional e intuitiva, com destaque para o sistema de validação de formulários e boas práticas de desenvolvimento.",
     languages: ["styled_components", "typescript", "react"],
@@ -49,7 +59,7 @@ const dataProjects: Project[] = [
   },
   {
     name: "Pomodoro",
-    image: "/images/pomodoro.png",
+    image: PomodoroImg,
     description:
       "Neste desafio, desenvolvi uma aplicação de timer Pomodoro para monitorar o tempo dedicado às atividades, incorporando um histórico detalhado das tarefas realizadas e seu status.",
     languages: ["styled_components", "typescript", "react"],
@@ -58,7 +68,7 @@ const dataProjects: Project[] = [
   },
   {
     name: "Página Vai na Web",
-    image: "/images/page-vnw.png",
+    image: PageVnwImg,
     description:
       "Neste projeto, realizei um teste técnico para uma oportunidade em uma empresa, onde fui responsável pelo desenvolvimento completo do layout, assegurando uma estrutura de código organizada e eficiente. Segui rigorosamente o design fornecido, incorporando melhorias e toques adicionais para aprimorar a experiência do usuário. Este projeto não apenas destacou minha capacidade de traduzir conceitos visuais em implementações funcionais, mas também foi um fator crucial que me ajudou a conquistar meu primeiro emprego na área.",
     languages: ["html", "styled_components", "javascript"],
